6401bb098196ca1fcf6d33f2133f88ee052ec2b5
apousette <apousette>
2021-09-10 14:09:48 +0200

    Skapade menyn, inmatningsprocessen, klassen för kundens egenskaper.

--- U210910X1/Program.cs
+++ U210910X1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace U210910X1
 {
@@ -6,8 +7,40 @@ namespace U210910X1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                //Vad som visas när programmet öppnas
+                Console.WriteLine("¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨");
+                Console.WriteLine("Du är nu inne i kund-systemet.");
+                Console.WriteLine("Vill du; \n1. Lägga till en ny kund, \n2. Se antalet kunder, \n3. Se en lista över alla kunder, \n4. Avsluta programmet?");
+                Console.WriteLine("");
+                Console.WriteLine("¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨");
 
+                //Inmatningsprocessen av kundens egenskaper.
+                Customer myCustomer;
+                myCustomer = new Customer();
+                myCustomer.Name = Console.ReadLine();
+                myCustomer.Phone = Console.ReadLine();
+                myCustomer.Adress = Console.ReadLine();
+
+                Console.WriteLine($"Ny kund tillagd: \nNamn: {myCustomer.Name} \nTelefonnummer: {myCustomer.Phone} \nAdress: {myCustomer.Adress}");
+
+                //Lista
+                List<Customer> MyCustomers = new List<Customer>();
+                MyCustomers.Add(myCustomer);
+
+
+            }
         }
     }
+
+    public class Customer
+    {
+        //Kundens olika egenskaper som matas in av användaren.
+        public string Name;
+        public string Phone;
+        public string Adress;
+
+    }
+
 }
