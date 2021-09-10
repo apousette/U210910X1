ce5239481de83074aeccb17fd2d6f541da1bb37c
apousette <apousette>
2021-09-10 12:19:19 +0200

    initial commit

---
+++ U210910X1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace U210910X1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
